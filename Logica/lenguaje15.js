daad14dbdbfb5633447855387d4103da90705a84
julianlpz69 <julianlpz69>
2023-06-02 15:57:50 -0500

    sunga

--- Logica/lenguaje15.js
+++ Logica/lenguaje15.js
@@ -5,34 +5,54 @@ const btn3 = document.getElementById('btn3');
 const contenedor = document.getElementById('contenedor-texto');
 
 
+
 let numeros = []
 let nombre = []
 let email = []
 let telefono = []
 
 
+function agregar(nombre,gmail,telefono){
+    let lugar = document.getElementById("tvody")
+    var parrafo = document.createElement("tr")
 
-btn1.addEventListener("click",function agregar(){
-
-    let nombres =(prompt("Escribe el nombre"))
-    nombre.push(nombres)
     
-    let emails =(prompt("Escribe el nombre"))
-    email.push(emails)
+    parrafo.innerHTML=
+        `<td>${nombre}</td>
+        <td>${gmail}</td>
+        <td>${telefono}</td>`;
 
-    let telefonos =(prompt("Escribe el nombre"))
-    telefono.push(telefonos)
+        lugar.appendChild(parrafo);
+}
+
+
+const formu = document.getElementById('formulario');
+
+btn1.addEventListener("click",function agregar(event){
+
+    event.preventDefault()
+   
+    const tell = document.getElementById('tell').value;
+    const nombres = document.getElementById('name').value;
+    const gmail = document.getElementById('gmail').value;
+
+    let lugar = document.getElementById("tvody")
+    var parrafo = document.createElement("tr")
+
     
-
-
-    let simulado = <tr>
-        <td>${telefonos}</td>
+    parrafo.innerHTML=
+        `<td>${numeros.length+1}</td>
         <td>${nombres}</td>
-        <td>${emails}</td>
-        <td>${telefonos}</td>
-    </tr>
+        <td>${gmail}</td>
+        <td>${tell}</td>`;
 
+        lugar.appendChild(parrafo);
+        numeros.push("uno")
 
     
-    })
+    formu.reset()
+})
 
+    
+
+    

9c1a835bb675feccb43e6930582bf8ee10af4384
Valerie <zharmel.afa@gmail.com>
2023-06-02 21:28:11 -0500

    CLASE DIFICIL

--- Logica/lenguaje15.js
+++ Logica/lenguaje15.js
@@ -1,58 +1,66 @@
 
 const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-const btn3 = document.getElementById('btn3');
+const form = document.getElementById('holiwi');
 const contenedor = document.getElementById('contenedor-texto');
+const padre = document.getElementById('tvody')
 
-
-
+contador = 1
 let numeros = []
-let nombre = []
-let email = []
-let telefono = []
 
 
-function agregar(nombre,gmail,telefono){
-    let lugar = document.getElementById("tvody")
-    var parrafo = document.createElement("tr")
 
-    
-    parrafo.innerHTML=
-        `<td>${nombre}</td>
-        <td>${gmail}</td>
-        <td>${telefono}</td>`;
-
-        lugar.appendChild(parrafo);
-}
 
 
 const formu = document.getElementById('formulario');
 
-btn1.addEventListener("click",function agregar(event){
+formu.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-    event.preventDefault()
-   
+btn1.addEventListener("click",function agregar(e){
+    e.preventDefault();
+
+    
+
     const tell = document.getElementById('tell').value;
     const nombres = document.getElementById('name').value;
     const gmail = document.getElementById('gmail').value;
 
     let lugar = document.getElementById("tvody")
     var parrafo = document.createElement("tr")
+    parrafo.id=`ids${contador}`
+    var btn2 = document.getElementById(`ids${contador}`);
+
+
+    
 
     
     parrafo.innerHTML=
         `<td>${numeros.length+1}</td>
         <td>${nombres}</td>
         <td>${gmail}</td>
-        <td>${tell}</td>`;
+        <td>${tell}</td>
+        <td><button id="holas${contador}">Eliminar</button></td>
+        `;
 
+        
+
+    var lol = `holas${contador}`
+    var lolo = `ids${contador}`
+            
         lugar.appendChild(parrafo);
         numeros.push("uno")
 
     
+
+
+
+    var btn2 = document.getElementById(lol);
+
+    btn2.addEventListener("click",function eliminar(){
+    var eliminar = document.getElementById(lolo)
+    padre.removeChild(eliminar)})
+
+    contador = contador + 1
+
     formu.reset()
-})
-
-    
-
-    
+})})

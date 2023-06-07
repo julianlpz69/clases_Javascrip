d69231d13168df479b67722c6b6c61322aa9c84b
Valerie <zharmel.afa@gmail.com>
2023-06-06 21:24:52 -0500

    locura extrema

--- Logica/lenguaje16.js
+++ Logica/lenguaje16.js
@@ -2,21 +2,39 @@ const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
 const btn4 = document.getElementById('btn4');
-const formulario1 = document.getElementById('formu2');
+const formulario2 = document.getElementById('formu2');
+const formulario11 = document.getElementById('formu11');
+const formulario12 = document.getElementById('formu12');
+const formulario13 = document.getElementById('formu13');
+const formulario3 = document.getElementById('formu3');
+const listap = document.getElementById('list')
+
+var lista = []
 
 
+let contador = 0
 
-let contador = 1
-
-function ola (){
-    formulario1.addEventListener('submit', function(event) {
+function agregar (){
+    formulario2.addEventListener('submit', function(event) {
     event.preventDefault();
     const titulo = document.getElementById('dato1').value;
     const genero = document.getElementById('dato2').value;
     const duracion = document.getElementById('dato3').value;
     const director = document.getElementById('dato4').value;
+    
+
+    var miMapa = new Map();
+
+    miMapa.set('titulo', titulo);
+    miMapa.set('genero', genero);
+    miMapa.set('duracion', duracion);
+    miMapa.set('director', director);
+
+    lista.push(miMapa)
+
     const lugar = document.getElementById("tvody")
     const parrafo = document.createElement("tr")
+    const lugar2 = document.getElementById("lista-peliculas")
 
     parrafo.id=`ids${contador}`
 
@@ -25,16 +43,38 @@ function ola (){
         <td>${genero}</td>
         <td>${duracion}</td>
         <td>${director}</td>
-        <td><button id="holas${contador}">Eliminar</button></td>
         `;
 
     lugar.appendChild(parrafo);
+
+    let parrafo2 = document.createElement("li")
+    
+    parrafo2.innerHTML = `${titulo}`
+    lugar2.appendChild(parrafo2);
+
     contador = contador+1
-    formulario1.reset()
+    formulario2.reset()
+})}
+
+// // Función para eliminar una película
+// function borrarPelicula(index) {
+//     peliculas.splice(index, 1);
+//     cargarPeliculas();
+//   }
+
+function botones (pametro1,parametro2){
+    parametro2.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    pametro1.classList.toggle('d-none')
+        
 })}
 
 
-btn4.addEventListener("click",ola())
+btn1.addEventListener("onclick" , botones(formulario2,formulario11))
 
+btn2.addEventListener("submit" , botones(listap,formulario12))
 
+btn3.addEventListener("submit" , botones(formulario3,formulario13))
 
+btn4.addEventListener("submit",agregar())
--- sintasis/mapas.js
+++ sintasis/mapas.js
@@ -1,6 +1,31 @@
 // Crear un nuevo Map
 const miMapa = new Map();
 
+
+
+// Un Mapa De Objetos IMPORTANTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+const miMapa2 = new Map();
+
+
+let titulo = "harry Poter"
+
+let holas = "peoo"
+let holas2 = "peoo2"
+let holas3 = "peoo3"
+
+miMapa2.set(titulo,{holas,holas2,holas3})
+
+console.log(miMapa2)
+
+console.log(miMapa2.get(titulo).holas2)
+
+console.log(miMapa2.has(titulo)) // ver si el valor existe en el mapa
+
+
+
+
+
+
 // Agregar elementos al Map
 miMapa.set('clave1', 'valor1');
 miMapa.set('clave2', 'valor2');
@@ -22,7 +47,6 @@ console.log('Valor de clave2:', miMapa.get('clave2'));
 // Agregar al mapa
 miMapa.set("clave4","valor4")
 console.log(miMapa);
-
 
 
 // Cambiar el valor de una clave
